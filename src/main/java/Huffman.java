e0783373565f03c3a2f401a4cd4575feb88de924
hediyehafzalzadeh <hedye.afzalzade@gmail.com>
2022-01-17 19:07:12 +0330

    huffman class

---
+++ src/main/java/Huffman.java
@@ -0,0 +1,47 @@
+public class Huffman {
+    static int all =0;
+
+
+    public void getMain(){
+        String main = Test.input.nextLine();
+        String [] mainSplit = main.split("");
+        all +=mainSplit.length;
+
+    }
+
+    public int checkRepetition(char x){
+        int xR = 0;
+        return xR;
+        //switch-case
+
+    }
+
+    public void makeTree(){
+        //for
+    }
+
+
+
+
+    public void huffmanTree(){
+
+        //recursive
+    }
+
+    public void characterCodding(){
+
+    }
+
+    public void codding(){
+        //whit buffer writer
+    }
+
+    public void readCodedFile(){
+        //whit buffer reader
+    }
+
+
+
+
+
+}
