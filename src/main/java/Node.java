f0eeb898cd7c109295e4839acf68ca32716e0880
hediyehafzalzadeh <hedye.afzalzade@gmail.com>
2022-01-17 19:08:16 +0330

    node class

---
+++ src/main/java/Node.java
@@ -0,0 +1,23 @@
+public class Node {
+    String character;
+    int repetition;
+
+    Node right;
+    Node left;
+
+    public String getCharacter() {
+        return character;
+    }
+
+    public int getRepetition() {
+        return repetition;
+    }
+
+    public void setCharacter(String character) {
+        this.character = character;
+    }
+
+    public void setRepetition(int repetition) {
+        this.repetition = repetition;
+    }
+}
