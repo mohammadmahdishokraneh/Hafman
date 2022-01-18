0797a3d310c54370b49a9d5b52b98f3245a10d68
hediyehafzalzadeh <hedye.afzalzade@gmail.com>
2022-01-18 16:30:09 +0330

    queue class

---
+++ src/main/java/Queue.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class Queue {
+    private ArrayList<Node> queue;
+    public Queue() {
+        this.queue = new ArrayList<Node>();
+    }
+    public void Enqueue(Node node){
+        queue.add(node);
+    }
+    public Node Dequeue() {
+        if (size()>0) {
+            return queue.remove(0);
+        }
+        return null;
+    }
+    public int size() {
+        return queue.size();
+    }
+
+}
