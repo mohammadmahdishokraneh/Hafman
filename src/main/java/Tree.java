2e82d771504f394ab63f5286b5b0ef1c357ba627
mohammadmahdishokraneh <mohammadmahdijavani1381@gmail.com>
2022-01-24 20:55:53 +0330

    Tree.java correction

--- src/main/java/Tree.java
+++ src/main/java/Tree.java
@@ -2,37 +2,14 @@ public class Tree {
     Node root;
 
     public Tree(Node root) {
-
+        this.root = root;
     }
 
-    public void insert(int repetition, String character, Node node) {
-        if (root == null)
-            root = new Node(character, repetition);
-        else {
-
-            if (repetition > node.repetition) {
-                if (node.right == null)
-                    node.right = new Node(character, repetition);
-                else
-                    insert(repetition, character, node.right);
-            } else {
-                if (node.left == null)
-                    node.right = new Node(character, repetition);
-                else
-                    insert(repetition, character, node.left);
-            }
-
-        }
-
+    public Node getRoot() {
+        return root;
     }
 
-    public void inorderTraversal(Node node) {
-        if (node != null) {
-            inorderTraversal(node.left);
-            System.out.println(node.repetition);
-            inorderTraversal(node.right);
-        }
+    public void setRoot(Node root) {
+        this.root = root;
     }
-
-
 }
