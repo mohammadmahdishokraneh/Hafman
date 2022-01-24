2642ce6ec00ea8573b94e7ab190e944c4d9a4db3
mohammadmahdishokraneh <mohammadmahdijavani1381@gmail.com>
2022-01-24 18:38:01 +0330

    add Element.java

---
+++ src/main/java/Element.java
@@ -0,0 +1,21 @@
+public class Element {
+
+    private String str;
+    private int number;
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
