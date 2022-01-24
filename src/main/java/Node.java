e1983b5d913a1606b1d0cf921af9eac595ce0b6b
mohammadmahdishokraneh <mohammadmahdijavani1381@gmail.com>
2022-01-24 21:40:20 +0330

    add merge in Node.java

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -5,6 +5,18 @@ public class Node {
     Node right;
     Node left;
 
+    public Node(String str, int number) {
+        this.str = str;
+        this.number = number;
+    }
+
+    public Node(String str, int number, Node right, Node left) {
+        this.str = str;
+        this.number = number;
+        this.right = right;
+        this.left = left;
+    }
+
     public String getStr() {
         return str;
     }
@@ -19,5 +31,9 @@ public class Node {
 
     public void setNumber(int number) {
         this.number = number;
+    }
+
+    public Node merge(Node node){
+        return new Node(this.str + node.str, this.number + node.number);
     }
 }
